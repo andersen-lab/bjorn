4d946034632ee6490b50bef66cad6a6df2b5d3db
AlaaALatif <al.a.latif.94@gmail.com>
2021-03-12 12:24:55 -0800

    bash script for running bjorn for outbreak.info

---
+++ bjorn_pipeline/run_sitrep_release.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd /home/al/code/bjorn/bjorn_pipeline
+source ~/anaconda3/etc/profile.d/conda.sh
+conda activate pipeline
+snakemake --cores 25 --config username=kga1978 password=Zb39Dnbk1dw --use-conda
