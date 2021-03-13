dae00eca0578b6fa04f6b6fb7be438ee5d0f0524
AlaaALatif <al.a.latif.94@gmail.com>
2021-03-13 13:35:09 -0800

    update bash

--- bjorn_pipeline/run_sitrep_release.sh
+++ bjorn_pipeline/run_sitrep_release.sh
@@ -2,4 +2,4 @@
 cd /home/al/code/bjorn/bjorn_pipeline
 source ~/anaconda3/etc/profile.d/conda.sh
 conda activate pipeline
-snakemake --cores 25 --config username=***REMOVED*** password=***REMOVED*** --use-conda
+snakemake --cores 15 --config username=***REMOVED*** password=***REMOVED*** --use-conda
