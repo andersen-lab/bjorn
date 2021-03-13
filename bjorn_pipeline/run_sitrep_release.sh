af9d26b8d415e19a4875d977ed6e98850e2ed932
AlaaALatif <al.a.latif.94@gmail.com>
2021-03-13 13:35:33 -0800

    _

--- bjorn_pipeline/run_sitrep_release.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd /home/al/code/bjorn/bjorn_pipeline
-source ~/anaconda3/etc/profile.d/conda.sh
-conda activate pipeline
-snakemake --cores 15 --config username=kga1978 password=Zb39Dnbk1dw --use-conda
