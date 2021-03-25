88a861abaa37a00f18e0fc0daa82fa2b0312bee0
AlaaALatif <al.a.latif.94@gmail.com>
2021-03-24 18:55:18 -0700

    added run with test inputs and outputs

--- run_alab_release.sh
+++ run_alab_release.sh
@@ -2,6 +2,6 @@
 # TEST RUN
 # python3 src/alab_release.py --min-coverage 95 --out-dir /valhalla/2021-03-17_release --sample-sheet /home/al/analysis/alab_release/SARS-CoV-2_sequence_tracker-GISAID.csv --cpus 25 --analysis-folder /valhalla/analysis --output-metadata /home/al/code/HCoV-19-Genomics/metadata.csv
 # DEV RUN
-python3 src/alab_release.py --min-coverage 95 --not-dry-run --include-bams --out-dir /valhalla/demo/release --sample-sheet /home/al/analysis/alab_release/SARS-CoV-2_sequence_tracker-GISAID.csv --cpus 25 --analysis-folder /valhalla/analysis --output-metadata /home/al/code/HCoV-19-Genomics/metadata.csv
+python3 src/alab_release.py --min-coverage 95 --not-dry-run --include-bams --out-dir /valhalla/tests/test_release --sample-sheet /valhalla/tests/test_analysis/SARS-CoV-2_sequence_tracker-GISAID.csv --cpus 25 --analysis-folder /valhalla/tests/test_analysis --output-metadata /valhalla/tests/test_analysis/metadata.csv
 # REAL RUN
 # python3 src/alab_release.py --min-coverage 95 --not-dry-run --include-bams --out-dir /valhalla/2021-03-17_release --sample-sheet /home/al/analysis/alab_release/SARS-CoV-2_sequence_tracker-GISAID.csv --cpus 25 --analysis-folder /valhalla/analysis --output-metadata /home/al/code/HCoV-19-Genomics/metadata.csv
