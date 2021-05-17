dae55182257348cc557960cb40ea0c3caa8d04f4
Alaa Abdel Latif <al.a.latif.94@gmail.com>
2021-05-17 13:51:01 -0700

    incorporate test mode with correct behavior, does NOT upload to outbreak nor generate an API file

--- src/merge_results.py
+++ src/merge_results.py
@@ -37,7 +37,7 @@ current_datetime = args.time
 
 with open('config.json', 'r') as f:
     config = json.load(f)
-
+is_test = config['feed_test']
 out_dir = config['out_dir']
 log_fp = out_dir + '/' + 'logs' + '/' + config['log_file'] + '_' + current_datetime + '.txt'
 unknown_val = config['unknown_value']
@@ -141,43 +141,46 @@ muts_info = ['type', 'mutation', 'gene',
              'deletion_codon_coords']
 muts = muts[~(muts['gene'].isin(['5UTR', '3UTR']))]
 muts['date_modified'] = date_modified
+# attach geolocation IDs based on GADM
 muts['country_id'] = muts['country'].apply(lambda x: countries.get(x, unknown_val)).astype(str)
 muts['tmp_info1'] = muts['country'] + '-' + muts['division']
 muts['division_id'] = muts['tmp_info1'].apply(lambda x: divisions.get(x, unknown_val)).astype(str)
 muts['tmp_info2'] = muts['country'] + '-' + muts['division'] + '-' + muts['location']
 muts['location_id'] = muts['tmp_info2'].apply(lambda x: locations.get(x, unknown_val)).astype(str)
-# muts['country_id'] = muts['country'].apply(lambda x: countries.get(x, unknown_val))
-# muts['division_id'] = muts['division'].apply(lambda x: divisions.get(x, unknown_val))
-# muts['location_id'] = muts['location'].apply(lambda x: locations.get(x, unknown_val))
+# drop record(s) with same sequence ID and mutation (redundancy cleaning)
 muts = muts.drop_duplicates(subset=['accession_id', 'mutation'])
 preprocess_time = time.time() - start
 # GENERATE JSON DATA MODEL
 start = time.time()
-(muts.groupby(meta_info, as_index=True)
+if not is_test:
+    (muts.groupby(meta_info, as_index=True)
              .apply(lambda x: x[muts_info].to_dict('records'))
              .reset_index()
              .rename(columns={0:'mutations'})
              .to_json(api_data_fp,
                       orient='records'))
-io_time = time.time() - start
-start = time.time()
-gzip_cmd = f"gzip -f {api_data_fp}"
-bs.run_command(gzip_cmd)
-api_data_fp += '.gz'
-gzip_time = time.time() - start
-# print(f'Execution time: {end - start} seconds')
-# upload to gcloud
-upload_cmd = f"/home/al/code/google-cloud-sdk/bin/gsutil -m cp {api_data_fp} gs://andersen-lab_temp/outbreak_genomics/"
-bs.run_command(upload_cmd)
-# update gcloud access rights
-upload_cmd = f"/home/al/code/google-cloud-sdk/bin/gsutil -m cp {meta_data_fp} gs://andersen-lab_temp/outbreak_genomics/"
-bs.run_command(upload_cmd)
-# send auto-slack message about it? (nah, too much)
-access_cmd = f"/home/al/code/google-cloud-sdk/bin/gsutil acl ch -R -u AllUsers:R gs://andersen-lab_temp/outbreak_genomics/*"
-bs.run_command(access_cmd)
-api_data_fn = api_data_fp.split('/')[-1]
-stat_cmd = f"/home/al/code/google-cloud-sdk/bin/gsutil stat gs://andersen-lab_temp/outbreak_genomics/{api_data_fn}"
-upload_stats = bs.run_command_log(stat_cmd)
+    io_time = time.time() - start
+    start = time.time()
+    gzip_cmd = f"gzip -f {api_data_fp}"
+    # bs.run_command(gzip_cmd)
+    api_data_fp += '.gz'
+    gzip_time = time.time() - start
+    # print(f'Execution time: {end - start} seconds')
+    # upload to gcloud
+    upload_cmd = f"/home/al/code/google-cloud-sdk/bin/gsutil -m cp {api_data_fp} gs://andersen-lab_temp/outbreak_genomics/"
+    # bs.run_command(upload_cmd)
+    # update gcloud access rights
+    upload_cmd = f"/home/al/code/google-cloud-sdk/bin/gsutil -m cp {meta_data_fp} gs://andersen-lab_temp/outbreak_genomics/"
+    # bs.run_command(upload_cmd)
+    # send auto-slack message about it? (nah, too much)
+    access_cmd = f"/home/al/code/google-cloud-sdk/bin/gsutil acl ch -R -u AllUsers:R gs://andersen-lab_temp/outbreak_genomics/*"
+    # bs.run_command(access_cmd)
+    api_data_fn = api_data_fp.split('/')[-1]
+    stat_cmd = f"/home/al/code/google-cloud-sdk/bin/gsutil stat gs://andersen-lab_temp/outbreak_genomics/{api_data_fn}"
+else:
+    io_time = "N/A"
+    gzip_time = "N/A"
+# upload_stats = bs.run_command_log(stat_cmd)
 # GENERATE CSV DATA MODEL
 num_records = muts.drop_duplicates(subset=meta_info).shape[0]
 num_ids = muts['accession_id'].unique().shape[0]
@@ -190,5 +193,5 @@ with open(log_fp, 'w') as f:
     f.write(f"Data Preprocessing Execution time: {preprocess_time} seconds\n")
     f.write(f"IO Execution time: {io_time} seconds\n")
     f.write(f"Gzip Execution time: {gzip_time} seconds\n")
-    f.write(f"GCloud upload statistics: \n {upload_stats}")
+    # f.write(f"GCloud upload statistics: \n {upload_stats}")
 print(f"Transfer Complete. All results saved in {out_dir}")
