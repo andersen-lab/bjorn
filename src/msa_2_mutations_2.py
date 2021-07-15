71be5345994f80bf1397215b3feb7e124f3bc59e
Ben Fry <ben@fathom.info>
2021-07-14 20:32:14 -0400

    break into main() and separate method for importing

--- src/msa_2_mutations_2.py
+++ src/msa_2_mutations_2.py
@@ -16,7 +16,6 @@ import data as bd
 
 
 ''' Hacked up from the version in bjorn_support to do xz.
-    TODO this leaves the file open
 '''
 def open_fasta_xz(fasta_filepath, is_aligned=False):
   handle = lzma.open(fasta_filepath, "rt")
@@ -31,36 +30,7 @@ def open_fasta_xz(fasta_filepath, is_aligned=False):
 
 ''' Modified to include insertions, and to write TSV instead of CSV. [fry 210713]
 '''
-if __name__=="__main__":
-  # COLLECTING USER PARAMETERS
-  parser = argparse.ArgumentParser()
-  parser.add_argument("-i", "--input",
-                          type=str,
-                          required=True,
-                          help="FASTA filepath containing aligned sequences")
-  # parser.add_argument("-m", "--meta",
-  #                         type=str,
-  #                         required=True,
-  #                         help="Gzipped TSV filepath containing sequence metadata")
-  parser.add_argument("-r", "--patient-zero",
-                          type=str,
-                          default="NC_045512.2",
-                          help="Sample name of reference sequence")
-  parser.add_argument("-d", "--data-src",
-                        type=str,
-                        default="gisaid_feed",
-                        help="Data source")
-  parser.add_argument("-o", "--outfp",
-                          type=str,
-                          required=True,
-                          help="Output filepath storing mutation information")
-  args = parser.parse_args()
-  alignment_filepath = args.input
-  # gisaid_meta = args.meta
-  patient_zero = args.patient_zero
-  data_src = args.data_src
-  out_fp = Path(args.outfp)
-
+def process_mutations(alignment_filepath, patient_zero, output_path, data_src='gisaid_feed'):
   print(f"Loading alignment file at {alignment_filepath}")
   t0 = time.time()
   # msa_data = bs.load_fasta(alignment_filepath, is_aligned=True, is_gzip=False)
@@ -70,38 +40,38 @@ if __name__=="__main__":
   print("Identifying insertions...")
   t0 = time.time()
   inserts, _ = bm.identify_insertions_per_sample(msa_data,
-                                            #  gisaid_meta,
-                                            gene2pos=bd.GENE2POS,
-                                            data_src=data_src,
-                                            patient_zero=patient_zero
-                                          #    test=is_test
-                                            )
+                                                 #gisaid_meta,
+                                                 gene2pos=bd.GENE2POS,
+                                                 data_src=data_src,
+                                                 patient_zero=patient_zero
+                                                 #test=is_test
+                                                 )
   inserts_time = time.time() - t0
 
   print("Identifying substitution-based mutations...")
   t0 = time.time()
   subs, _ = bm.identify_replacements_per_sample(msa_data,
-                                                # gisaid_meta,
+                                                #gisaid_meta,
                                                 gene2pos=bd.GENE2POS,
                                                 data_src=data_src,
                                                 min_seq_len=20000,
                                                 patient_zero=patient_zero
-                                              #   test=is_test
+                                                #test=is_test
                                                 )
   subs_time = time.time() - t0
 
   print("Identifying deletion-based mutations...")
   t0 = time.time()
   dels, _ = bm.identify_deletions_per_sample(msa_data,
-                                            #  gisaid_meta,
-                                            gene2pos=bd.GENE2POS,
-                                            data_src=data_src,
-                                            min_del_len=1,
-                                            max_del_len=500,
-                                            min_seq_len=20000,
-                                            patient_zero=patient_zero
-                                          #    test=is_test
-                                            )
+                                             #gisaid_meta,
+                                             gene2pos=bd.GENE2POS,
+                                             data_src=data_src,
+                                             min_del_len=1,
+                                             max_del_len=500,
+                                             min_seq_len=20000,
+                                             patient_zero=patient_zero
+                                             #test=is_test
+                                             )
   dels_time = time.time() - t0
 
   # QC FILTER: remove seqs with >500 nt deletions
@@ -118,10 +88,31 @@ if __name__=="__main__":
   # muts_filename = alignment_filepath.replace('.aligned.fasta', f'_{date}.mutations.csv')
 
   # put columns into alphabetical order, because the ordering seems to be arbitrary,
-  # and this at least keeps them in some sort of comparable order
+  # and this at least keeps them in some sort of comparable order between runs
   muts.sort_index(axis=1, inplace=True)
   # muts.to_csv(out_fp, index=False)  # old version was CSV, so we'll keep using that; oh well
   with lzma.open(out_fp, 'wt') as ofp:
     muts.to_csv(ofp, sep='\t', quoting=csv.QUOTE_NONE, index=False)  # TSV version
 
   print(f"Mutations extracted from {alignment_filepath} and saved in {out_fp}\n")
+
+  # TODO should be earlier; how soon can this be closed?
+  msa_data.close()
+
+
+if __name__=="__main__":
+  # COLLECTING USER PARAMETERS
+  parser = argparse.ArgumentParser()
+  parser.add_argument("-i", "--input", type=str, required=True, help="FASTA filepath containing aligned sequences")
+  # parser.add_argument("-m", "--meta", type=str, required=True, help="Gzipped TSV filepath containing sequence metadata")
+  parser.add_argument("-r", "--patient-zero", type=str, default="NC_045512.2", help="Sample name of reference sequence")
+  parser.add_argument("-d", "--data-src", type=str, default="gisaid_feed", help="Data source")
+  parser.add_argument("-o", "--outfp", type=str, required=True, help="Output filepath storing mutation information")
+  args = parser.parse_args()
+  alignment_filepath = args.input
+  # gisaid_meta = args.meta
+  patient_zero = args.patient_zero
+  data_src = args.data_src
+  out_fp = Path(args.outfp)
+
+  process_mutations(alignment_filepath, patient_zero, data_src, out_fp)
