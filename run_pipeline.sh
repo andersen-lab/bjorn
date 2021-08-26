a6265978effc3cf1111f6ec5dd8abdc6cb127972
Emory Hufbauer <mindoftea@gmail.com>
2021-08-26 16:59:18 -0700

    cleanup

--- run_pipeline.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-source ~/anaconda3/etc/profile.d/conda.sh
-conda activate bjorn
-snakemake --configfile $1 clear
-snakemake --cores 20 --configfile $1 all upload
--- scripts/convert_to_unaigned_fasta.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-fasta=$1
-awk '{if($0 ~ "^>"){print $0;}else{gsub("-", "", $0);print $0;}}' $fasta
-
-
