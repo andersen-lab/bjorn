652177fd333091d518e2329ef3e2112427665c26
Emory Hufbauer <mindoftea@gmail.com>
2021-08-26 16:59:18 -0700

    cleanup

--- scripts/tview.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# echo $(find ./ -name *$1*.trimmed.sorted.bam)
-samtools tview --reference /home/gk/code/hCoV19/db/NC045512.fasta -p NC_045512.2:$2 $(find ./ -name *$1*.trimmed.sorted.bam)
