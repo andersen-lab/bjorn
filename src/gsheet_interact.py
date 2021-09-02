2ad8769646a3f5c1d94bc58551b1f6fe9e0523a9
Karthik Ramesh <karthikramesh@Karthiks-MacBook-Pro.local>
2021-09-02 06:20:05 -0700

    added gsheets file and updated env

---
+++ src/gsheet_interact.py
@@ -0,0 +1,18 @@
+"""
+Interact with gsheets for automation sake
+"""
+
+import pandas as pd
+
+def get_gsheet():
+    """
+    get from gsheet
+    """
+    pass
+
+def push_gsheet():
+    """
+    push data to gsheet
+    """
+    pass
+
