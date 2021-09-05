65f00e84d2e928cd7c425604a93bdec10e068c07
Karthik Ramesh <kramesh@scripps.edu>
2021-09-05 15:12:04 -0700

    adjusting metadata path

--- run_alab_release.sh
+++ run_alab_release.sh
@@ -4,4 +4,4 @@
 # DEV RUN
 # python3 src/alab_release.py --min-coverage 95 --min-depth 1000 --not-dry-run --include-bams --out-dir /valhalla/tests/test_release --sample-sheet /valhalla/tests/test_analysis/SARS-CoV-2_sequence_tracker-GISAID.csv --cpus 25 --analysis-folder /valhalla/tests/test_analysis --output-metadata /valhalla/tests/test_analysis/metadata.csv
 # REAL RUN
-python3 src/alab_release.py --min-coverage 95 --min-depth 1000 --not-dry-run --include-bams --out-dir /home/kramesh/test_release --sample-sheet /home/kramesh/test_release/metadata.csv --cpus 25 --analysis-folder /home/kramesh/test_analysis --output-metadata /home/kramesh/HCoV-19-Genomics/metadata.csv
+python3 src/alab_release.py --min-coverage 95 --min-depth 1000 --not-dry-run --include-bams --out-dir /home/kramesh/test_release --sample-sheet /home/kramesh/metadata.csv --cpus 25 --analysis-folder /home/kramesh/test_analysis --output-metadata /home/kramesh/HCoV-19-Genomics/metadata.csv
