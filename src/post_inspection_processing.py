21012c9288325183bf4b6ab352c257b6ff89732d
kramesh1 <43083703+kramesh1@users.noreply.github.com>
2021-09-06 07:29:15 -0700

    move readme update code to HCoV-19-Genomics

--- src/post_inspection_processing.py
+++ src/post_inspection_processing.py
@@ -9,28 +9,3 @@ bash ~/code/bjorn/scripts/multifasta_to_fasta.sh ../2021-08-25_release_unaligned
 with a single step process
 """
 
-import pandas as pd
-
-def generate_location_summary_table(metadata: str, out_file: str, location_combination_config: str) -> None:
-    """
-    Get original ReadME 
-    Generate a file based on metadata to get the summary
-    Map similar locations
-    Generate a new readme for the HCoV-19-Genomics Repo
-    Return that new ReadME
-    """
-    # read file
-    df = pd.read_csv(metadata)
-    # generate locations df
-    locations = pd.DataFrame(df.location.value_counts())
-    locations.index.rename("Location", inplace=True)
-    locations.rename(columns={"location": "Number of Sequences"}, inplace=True)
-    # 
-    # write dataframe to a str
-    locations.to_markdown(out_file)
-    return
-
-def update_and_combine_readme(metadata_md: str, readme_md: str):
-    """
-    Takes the normal readme, updates it with new table info
-    """
