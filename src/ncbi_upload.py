98fe1e1720b870917cfcbebb44b820bf82f95260
kramesh1 <43083703+kramesh1@users.noreply.github.com>
2021-09-14 20:58:01 -0700

    added wastewater separation func and linted

--- src/ncbi_upload.py
+++ src/ncbi_upload.py
@@ -2,17 +2,22 @@
 Use these utils to upload sequences to NCBI
 """
 
-import pandas as pd
-from gsheet_interact import gisaid_interactor
-
+import os
+import shutil
 from typing import Dict, List
 
-def convert_metadata(metadata: pd.DataFrame, output_mapping: Dict[str, str]) -> pd.DataFrame:
+import pandas as pd
+
+
+def convert_metadata(
+    metadata: pd.DataFrame, output_mapping: Dict[str, str]
+) -> pd.DataFrame:
     """
     Takes the metadata given and converts it into the ncbi
     format
     """
     pass
+
 
 def batch_by_author(metadata: pd.DataFrame) -> List[pd.DataFrame]:
     """
@@ -21,20 +26,48 @@ def batch_by_author(metadata: pd.DataFrame) -> List[pd.DataFrame]:
     """
     pass
 
+
+def separate_wastewater(
+    wastewater_sample_list_path: str,
+    all_sequences_location: str,
+    separated_location: str,
+) -> None:
+    """
+    Pull out the relevant wastewater sequences for separate processing
+    """
+    # load the list of samples from a data file
+    with open(wastewater_sample_list_path, "r") as infile:
+        lines = infile.readlines()
+        wastewater_samples_list = [line.rstrip() for line in lines]
+
+    # in the location specified, search for these files and extract them into a separate location
+    if not os.path.exists(separated_location):
+        os.mkdir(separated_location)
+
+    for sequence_file in wastewater_samples_list:
+        shutil.copy(
+            os.path.join(all_sequences_location, sequence_file), separated_location
+        )
+
+    return wastewater_samples_list
+
+
 def extract_sequences():
     """
     Not quite sure how this would work exactly, but it would need to compare what
     we have already uploaded vs what we have left to upload and then return a
-    relevant sheet of the associated metadata with those sequences and then sequences 
+    relevant sheet of the associated metadata with those sequences and then sequences
     / bam files themselves.
     """
     pass
+
 
 def extract_bam_files():
     """
     Same as above - maybe this is separate from bam files and sequences?
     """
     pass
+
 
 def generate_bucket_list():
     """
@@ -45,7 +78,9 @@ def generate_bucket_list():
     pass
 
 
-def map_biosample_ids(metadata: pd.DataFrame, biosample_data: pd.DataFrame, sequences: str, bam_files: str) -> None:
+def map_biosample_ids(
+    metadata: pd.DataFrame, biosample_data: pd.DataFrame, sequences: str, bam_files: str
+) -> None:
     """
     Need to take the biosample data, our metadata, and use all of that to map the sequences and the bam file for upload
     This should ideally happen after batching by author, but doesn't matter a ton?
