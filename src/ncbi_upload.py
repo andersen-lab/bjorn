7415d91058bb477eda18ca1feda92ac0af728e98
kramesh1 <43083703+kramesh1@users.noreply.github.com>
2021-09-16 09:48:14 -0700

    adding basic conversion functionality and metadata combination

--- src/ncbi_upload.py
+++ src/ncbi_upload.py
@@ -5,17 +5,40 @@ Use these utils to upload sequences to NCBI
 import os
 import shutil
 from typing import Dict, List
+import json
 
 import pandas as pd
 
+from gsheet_interact import gisaid_interactor
+
+def merge_metadata(repo_metadata: pd.DataFrame, online_metadata: pd.DataFrame) -> pd.DataFrame:
+    """
+    Takes the online metadata and remerges it with that published in the HCoV-19 repo
+    to create a combined file with all needed columns
+    """
+    merged_data = repo_metadata.merge(online_metadata, how="left", left_on="fasta_hdr", right_on="Virus name")
+    return merged_data
+
 
 def convert_metadata(
-    metadata: pd.DataFrame, output_mapping: Dict[str, str]
+    metadata: pd.DataFrame,  
+    column_config_file: str,
+    constant_config_file: str,
+    wastewater: bool = False,
 ) -> pd.DataFrame:
     """
     Takes the metadata given and converts it into the ncbi
     format
     """
+    # load the config file
+    if wastewater:
+        column_mapping = json.loads(column_config_file)["wastewater"]
+        constant_mapping = json.loads(constant_config_file)["wastewater"]
+    else:
+        column_mapping = json.loads(column_config_file)["normal"]
+        constant_mapping = json.loads(constant_config_file)["normal"]
+    
+    # convert columns
     pass
 
 
@@ -49,7 +72,31 @@ def separate_wastewater(
             os.path.join(all_sequences_location, sequence_file), separated_location
         )
 
-    return wastewater_samples_list
+    return
+
+def separate_normal(
+    normal_sample_list_path: str,
+    all_sequences_location: str,
+    separated_location: str
+) -> None:
+    """
+    Pull out the relevant normal sequences for separate processing
+    """
+    # load the list of samples from a data file
+    with open(normal_sample_list_path, "r") as infile:
+        lines = infile.readlines()
+        normal_samples_list = [line.rstrip() for line in lines]
+
+    # in the location specified, search for these files and extract them into a separate location
+    if not os.path.exists(separated_location):
+        os.mkdir(separated_location)
+
+    for sequence_file in normal_samples_list:
+        shutil.copy(
+            os.path.join(all_sequences_location, sequence_file), separated_location
+        )
+
+    return 
 
 
 def extract_sequences():
